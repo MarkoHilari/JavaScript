80b08e1f2e76b4bd6665d0c785488a5bc43ff342
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-09-27 12:53:50 +0300

    Javaa

---
+++ JSviikko2tehtävät/tehtava1.js
@@ -0,0 +1,48 @@
+function numero()
+{
+  var luku = document.getElementById('l1').value
+  if(luku <= 0)
+  {
+    document.getElementById('v1').innerHTML = "Luku on negatiivinen";
+  }
+  else {
+    document.getElementById('v1').innerHTML = "luku on positiivinen";
+  }
+}
+
+function paiva()
+{
+  var viikonpaiva = document.getElementById('pp').value;
+  switch (viikonpaiva)
+  {
+    case "1":
+      document.getElementById('v1').innerHTML = ("Päivä on Maanantai");
+      break;
+    case "2":
+      document.getElementById('v1').innerHTML = ("Päivä on Tiistai");
+      break;
+    case "3":
+      document.getElementById('v1').innerHTML = ("Päivä on Keskiviikko");
+      break;
+    case "4":
+      document.getElementById('v1').innerHTML = ("Päivä on Torstai");
+      break;
+    case "5":
+      document.getElementById('v1').innerHTML = ("Päivä on Perjantai");
+      break;
+      case "6":
+      document.getElementById('v1').innerHTML = ("Päivä on Lauantai");
+      break;
+      case "7":
+      document.getElementById('v1').innerHTML = ("Päivä on Sunnuntai");
+      break;
+    default:
+      document.getElementById('v1').innerHTML = "väärä tieto";
+  }
+}
+
+function luku()
+{
+  var l1, l2, l3, l4, l5
+  
+}
