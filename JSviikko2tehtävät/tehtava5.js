2a0c587d09073e1b665289e1e74d9b0d7ccdbc08
MarkoHilari <marko.hilari@edu.keuda.fi>
2022-09-29 11:30:34 +0300

    T5

--- JSviikko2tehtävät/tehtava5.js
+++ JSviikko2tehtävät/tehtava5.js
@@ -1,5 +1,12 @@
 function lause()
 {
-var luku = document.getElementById('lu').value
-
+  var luku = document.getElementById('lu').value;
+  var temp = 0;
+  var vastaus = "";
+  for(var i = 1; i <= 10; i++)
+  {
+    temp = luku * i;
+    vastaus += "luku " + luku + " * " + i + " = " + temp +"<br />";
+  }
+  document.getElementById('kertotaulu').innerHTML = vastaus;
 }
